6b6b7c5698733aed0cc238a76d3841cb292dd50e
Harley <harleyor27@gmail.com>
2024-10-28 22:45:37 -0300

    Adicionando primeira questao

---
+++ ex001.py
@@ -0,0 +1,10 @@
+#declarando as variáveis de controle
+indice = 13
+soma = 0
+k = 0
+#loop para achar o valor da variável soma
+while k < indice:
+    k += 1 
+    soma = soma + k
+
+print(soma) #mostrando o valor da variável soma
