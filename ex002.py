3c80e96e77e1ae0fba84add1f73631c31c602c38
Harley <harleyor27@gmail.com>
2024-10-28 23:00:36 -0300

    Organizando melhor

--- ex002.py
+++ ex002.py
@@ -1,8 +1,10 @@
 num= int(input("Informe um número para verificar se pertencia a sequência de Fibonacci: ")) #solicita o numero ao usuario 
+
 #inicializando a variaveis como dois primeiros numeros da sequencia
 n1 = 0
 n2 = 1
 pertence = False #variavel de controle para saber se pertence ou nao
+
 #verifica se o numero informador pertence a sequencia
 while n1 <= num:
 
@@ -15,6 +17,7 @@ while n1 <= num:
     n1 = n2
 
     n2 = temp + n2
+    
 #mostra o resultado desejado
 if pertence:
     print(f"O número {num}, pertence à sequência de Fibonacci.")
